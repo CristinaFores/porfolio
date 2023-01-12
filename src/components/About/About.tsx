04b1418bf22a7075e94a7ae6d451b2c4beeb71f4
Cristina <cristinaforescampos1992@gmail.com>
2023-01-12 23:39:22 +0100

    Add link cv

--- src/components/About/About.tsx
+++ src/components/About/About.tsx
@@ -1,12 +1,15 @@
 import { useState } from "react";
+import { Link } from "react-router-dom";
 import Button from "../Button/Button";
 import {
   AboutStyled,
   AboutAvatarStyled,
   AboutButtonStyled,
+  TextSpanStyled,
 } from "./AboutStyled";
 const article = {
-  disc: "I have been working on small and medium projects, developing skills in both front-end and back-end, such as Nodejs. I have also learned about the use of non-relational databases such as MongoDB, and have developed skills in implementing secure and scalable web applications. I love to learn and am always looking for new opportunities to improve my skills.",
+  bio: "I have been working on small and medium projects, developing skills in both front-end and back-end, such as Nodejs. I have also learned about the use of non-relational databases such as MongoDB, and have developed skills in implementing secure and scalable web applications. I love to learn and am always looking for new opportunities to improve my skills.",
+  link: "Download CV",
 };
 const About = (): JSX.Element => {
   const [showText, setshowText] = useState(false);
@@ -24,7 +27,12 @@ const About = (): JSX.Element => {
         Full-stack
         <b> Web developer </b>
       </h3>
-      <p>{showText ? article.disc : article.disc.substring(0, 0) + ""}</p>
+      <TextSpanStyled>
+        <p>{showText ? article.bio : article.bio.substring(0, 0) + ""}</p>
+        <Link to={"/project"}>
+          {showText ? article.link : article.bio.substring(0, 0) + ""}
+        </Link>
+      </TextSpanStyled>
       <AboutButtonStyled>
         <Button
           action={() => setshowText(!showText)}
