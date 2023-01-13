61f965414b689f9486e2d188403dc9763d566f72
Cristina <cristinaforescampos1992@gmail.com>
2023-01-13 23:05:50 +0100

    Change Navbar scroll

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,12 +1,12 @@
-import { Link } from "react-router-dom";
+import { Link } from "react-scroll";
 import { NavbarStyled } from "./NavbarStyled";
 
 const Navbar = (): JSX.Element => {
   return (
     <NavbarStyled>
-      <Link to={"/about"}>About me</Link>
-      <Link to={"/projects"}>Projects</Link>
-      <Link to={"/contact"} className="contact">
+      <Link to={"about"}>About me</Link>
+      <Link to={"projects"}>Projects</Link>
+      <Link to={"contact"} className="contact">
         Contact
       </Link>
     </NavbarStyled>
