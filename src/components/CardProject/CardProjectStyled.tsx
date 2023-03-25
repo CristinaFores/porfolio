83bced51ab6283b380bc75c3417e85c95d0db93e
Cristina <cristinaforescampos1992@gmail.com>
2023-03-25 11:15:39 +0100

    Fix card projects width

--- src/components/CardProject/CardProjectStyled.tsx
+++ src/components/CardProject/CardProjectStyled.tsx
@@ -4,7 +4,7 @@ export const CardProjectStyled = styled.div`
   box-shadow: rgba(50, 50, 105, 0.15) 0px 2px 5px 0px,
     rgba(0, 0, 0, 0.05) 0px 1px 1px 0px;
   border-radius: 5px;
-  width: 80%;
+  width: 100%;
 
   h4 {
     font-size: 1.5rem;
